75f19d81a7908134de63c8276a8df70ba08278f9
Dan McCune <megadannn@gmail.com>
2017-12-22 11:02:11 -0600

    bare bones

---
+++ src/Skills.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Skills.css';
+
+class Skills extends Component {
+    render() {
+      return (
+        <div className="skills">
+            skills
+        </div>
+      );
+    }
+  }
+
+export default Skills;
