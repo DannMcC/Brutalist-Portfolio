252641cb21d86e5e4a6e8f0307657d5ed33d33ab
Dan McCune <megadannn@gmail.com>
2018-01-10 10:19:24 -0600

    styling ornament

--- src/Ornament.js
+++ src/Ornament.js
@@ -5,9 +5,7 @@ class Ornament extends Component {
     render() {
       return (
         <div className="ornaments">
-            <marquee className="ornament-1">test</marquee>
-            <marquee className="ornament-2">test</marquee>
-            <marquee className="ornament-3">test</marquee>
+            <div className="marquee"></div>
         </div>
       );
     }
